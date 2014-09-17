02a61be81b3662993d4de166abe56df42f7f1a7e
HugoKuo <tonytkdk@gmail.com>
2014-09-17 23:36:34 +0800

    Pin the sqlalchemy-migrate to 0.9.1

--- install_keystone.sh
+++ install_keystone.sh
@@ -15,6 +15,9 @@ hash -r
 pip install --upgrade pbr
 pip install python-keystoneclient==0.9.0
 
+#To pin the version to 0.9.1. Due to 0.9.2 has bug.
+pip install sqlalchemy-migrate==0.9.1
+
 #For compiling dependencies of several pip libraries , you need to install following packages first
 apt-get install -y gcc python-dev libxml2-dev libxslt-dev
 
